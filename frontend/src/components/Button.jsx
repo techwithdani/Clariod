f9c3b18fa30c0082dbf797cda04d229b0aa1ac64
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-02-25 15:36:52 +0500

    Create button component

---
+++ frontend/src/components/Button.jsx
@@ -0,0 +1,13 @@
+const Button = ({ text }) => {
+  return (
+    <>
+      <div className="w-40 h-12 bg-blue flex justify-center items-center rounded-3xl hover:bg-white hover:text-blue transition-all duration-500">
+        <button type="button" className="text-[1.1rem] font-semibold cursor-pointer">
+          {text}
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Button;
--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -1,5 +1,6 @@
 import heroImg1 from "../assets/heroImg1.jpg";
 import heroImg2 from "../assets/heroImg2.jpg";
+import Button from "./Button";
 
 const HeroSection = () => {
   return (
@@ -7,7 +8,9 @@ const HeroSection = () => {
       <section>
         <div className="bg-darkBlue text-white text-balance w-full pt-8 md:flex flex-wrap justify-evenly">
           <div className="w-96 pl-8 mt-6 md:w-1/2 md:pl-17 lg:pl-29 xl:pr-21 2xl:pr-52">
-            <h1 className="text-4xl font-semibold md:text-[3.10rem] lg:text-6xl xl:text-7xl">Discover The Vision</h1>
+            <h1 className="text-4xl font-semibold md:text-[3.10rem] lg:text-6xl xl:text-7xl">
+              Discover The Vision
+            </h1>
           </div>
           <div className="w-96 pl-8 mt-6 md:w-1/2 md:pl-0 md:pr-21 xl:pr-39 2xl:pr-52">
             <p className="text-[1.13rem] md:text-[1.30rem] lg:text-[1.40rem] xl:text-2xl">
@@ -16,12 +19,12 @@ const HeroSection = () => {
               processes & elevate your business.
             </p>
             <div className="mt-8">
-              <button>Learn More</button>
+              <Button text={"Learn More"} />
             </div>
           </div>
           <div className="w-full mt-16 overflow-hidden md:flex">
-            <img src={heroImg1} alt="Hero Img 1" className="md:w-1/2"/>
-            <img src={heroImg2} alt="Hero Img 2" className="md:w-1/2"/>
+            <img src={heroImg1} alt="Hero Img 1" className="md:w-1/2" />
+            <img src={heroImg2} alt="Hero Img 2" className="md:w-1/2" />
           </div>
         </div>
       </section>
