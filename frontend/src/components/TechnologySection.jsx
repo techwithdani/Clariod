498b75f8ec1f0270430d84d8da93cda181e5280b
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-02-26 15:18:24 +0500

    Create technology section

---
+++ frontend/src/components/TechnologySection.jsx
@@ -0,0 +1,26 @@
+const TechnologySection = () => {
+  return (
+    <>
+      <section>
+        <div>
+          <div>
+            <h1>Technology</h1>
+          </div>
+          <div>
+            <p>Innovative Solutions</p>
+          </div>
+          <div>
+            <p>
+              Our technology stack is designed to meet the diverse needs of
+              modern businesses. From cloud-based applications to AI-driven
+              analytics, we leverage the latest technologies to deliver seamless
+              and effective solutions.
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default TechnologySection;
