555b44dd6854ab7c6a6b7761e1b6b9cfbfea2188
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-03-02 12:53:02 +0500

    Change responsive design

--- frontend/src/components/QuoteSection.jsx
+++ frontend/src/components/QuoteSection.jsx
@@ -6,19 +6,19 @@ const QuoteSection = () => {
     <>
       <section>
         <div className="bg-darkBlue text-white">
-          <div className="pt-15 pl-8">
+          <div className="pt-15 pl-8 md:pl-17 lg:pl-29">
             <h1 className="text-4xl font-semibold">Quote</h1>
           </div>
           <HorizontalLine />
-          <div className="mt-8 pl-8 pr-10">
-            <p className="text-[1.2rem]">
+          <div className="mt-8 pl-8 pr-10 md:pl-17 md:pr-30 lg:pl-29 lg:pr-52 xl:ml-76 xl:pr-96">
+            <p className="text-[1.2rem] lg:text-[1.4rem]">
               &quot;Clariod has truly transformed our business operations. Their
               innovative software solutions have increased our efficiency and
               productivity, allowing us to focus on what truly matters – driving
               growth and success.&quot;
             </p>
           </div>
-          <div className="pl-8 mt-6 flex items-center gap-3">
+          <div className="pl-8 mt-6 flex items-center gap-3 md:pl-17 lg:pl-29 xl:ml-76">
             <img src={quoteImg} alt="Quote Image" />
             <p>Rona El, CEO & Vailu</p>
           </div>
