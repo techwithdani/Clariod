d6f1cc5338b8ee1a5c1f63da544862e6e306cb9c
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-03-03 09:10:25 +0500

    Fix responsive design

--- frontend/src/components/KeyBenefitsType.jsx
+++ frontend/src/components/KeyBenefitsType.jsx
@@ -1,15 +1,15 @@
 const KeyBenefitsType = ({ image, title, subTitle, paragraph }) => {
   return (
     <>
-      <div className="pl-8 mt-6 md:pl-17 lg:pl-29 xl:flex justify-between items-center">
-        <div className="flex items-center gap-3 xl:w-57">
+      <div className="pl-8 mt-6 md:pl-17 lg:pl-29 lg:flex justify-between items-center gap-8 lg:mt-0">
+        <div className="flex items-center gap-3 lg:w-57">
           <img src={image} alt={title} />
           <h3 className="text-[1.3rem]">{title}</h3>
         </div>
-        <div className="mt-8 xl:mt-0 xl:w-50">
+        <div className="mt-8 lg:mt-0 lg:w-50">
           <p className="text-[1.2rem]">{subTitle}</p>
         </div>
-        <div className="my-5 pr-20 lg:pr-40 xl:w-1/2">
+        <div className="my-5 pr-20 md:pr-40 lg:w-1/2 xl:pr-50">
           <p className="text-[1.1rem]">{paragraph}</p>
         </div>
       </div>
