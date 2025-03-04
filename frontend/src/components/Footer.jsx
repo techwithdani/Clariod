4772c1a56a7876279ed757ef2367acbdeb1d403c
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-03-04 14:49:12 +0500

    Change responsive design

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,32 +2,32 @@ const Footer = () => {
   return (
     <>
       <footer>
-        <div className="bg-darkBlue text-white xl:grid grid-cols-4">
-          <div className="w-55 pl-8 pt-10 md:pl-17 lg:pl-29">
-            <div>
+        <div className="bg-darkBlue text-white xl:grid grid-cols-4 xl:pb-40">
+          <div className="pl-8 pt-10 md:pl-17 lg:pl-29">
+            <div className="w-55">
               <p className="text-3xl font-semibold">Claroid</p>
               <p className="text-[1.3rem]">Discover The Vision</p>
             </div>
           </div>
-          <div className="w-55 pl-8 mt-6 md:pl-17 lg:pl-29 col-start-3 xl:pl-0 xl:mt-0 xl:flex items-center justify-center">
-            <div>
+          <div className="pl-8 mt-6 md:pl-17 lg:pl-29 col-start-3 xl:pl-0 xl:mt-0 xl:flex items-center justify-center">
+            <div className="w-55">
               <p>123-986-7653</p>
               <p>claroid@gmail.com</p>
             </div>
           </div>
-          <div className="w-55 pl-8 mt-3 md:pl-17 md:w-65 lg:pl-29 lg:w-auto col-start-4">
-            <div>
+          <div className="pl-8 mt-3 md:pl-17 md:w-65 lg:pl-29 lg:w-auto col-start-4 xl:pl-0 xl:mt-0 xl:flex items-center justify-start">
+            <div className="w-55">
               <p>500 Terry Francine Street, San Francisco, CA 94158</p>
             </div>
           </div>
-          <div className="w-55 pl-8 mt-6 md:pl-17 lg:pl-29 row-start-2 col-start-3">
-            <div>
+          <div className="pl-8 mt-6 md:pl-17 lg:pl-29 row-start-2 col-start-3 xl:pl-0 xl:mt-0 xl:flex items-center justify-center">
+            <div className="w-55">
               <p>Privacy Policy</p>
               <p>Accessibility Statement</p>
             </div>
           </div>
-          <div className="w-65 pl-8 pb-5 mt-8 md:pl-17 lg:pl-29 row-start-2 col-start-4">
-            <div>
+          <div className="pl-8 pb-5 mt-8 md:pl-17 lg:pl-29 row-start-2 col-start-4 xl:pl-0 xl:pb-0 xl:mt-0 xl:flex items-center justify-start">
+            <div className="w-65">
               <p>© 2035 by Clariod. Powered and secured by Claroid</p>
             </div>
           </div>
