b9e2b961ba13adbc760a50c89a4607e827c4de23
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-03-04 14:07:57 +0500

    Change responsive design

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,26 +3,24 @@ const Footer = () => {
     <>
       <footer>
         <div className="bg-darkBlue text-white">
-          <div className="pl-8 pt-10">
+          <div className="pl-8 pt-10 md:pl-17 lg:pl-29">
             <p className="text-3xl font-semibold">Claroid</p>
-          </div>
-          <div className="pl-8 mt-2">
             <p className="text-[1.3rem]">Discover The Vision</p>
           </div>
-          <div className="pl-8 mt-6">
+          <div className="pl-8 mt-6 md:pl-17 lg:pl-29">
             <p>123-986-7653</p>
             <p>claroid@gmail.com</p>
           </div>
-          <div className="w-55 pl-8 mt-3">
+          <div className="w-55 pl-8 mt-3 md:pl-17 md:w-65 lg:pl-29 lg:w-auto">
             <p className="">
               500 Terry Francine Street, San Francisco, CA 94158
             </p>
           </div>
-          <div className="pl-8 mt-6">
+          <div className="pl-8 mt-6 md:pl-17 lg:pl-29">
             <p>Privacy Policy</p>
             <p>Accessibility Statement</p>
           </div>
-          <div className="pl-8 pb-5 mt-8">
+          <div className="pl-8 pb-5 mt-8 md:pl-17 lg:pl-29">
             <p>© 2035 by Clariod. Powered and secured by Claroid</p>
           </div>
         </div>
