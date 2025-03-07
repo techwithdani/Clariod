de06832089e914cf27bcdeb2a9f5030c7dc40a7a
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-03-07 16:02:14 +0500

    Swap between layout on change in screen size

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,4 +1,9 @@
+import useWindowDimension from "../hooks/useWindowDimension";
+import Menu from "./Menu";
+
 const NavBar = () => {
+  const getWidth = useWindowDimension();
+
   return (
     <>
       <nav>
@@ -7,22 +12,26 @@ const NavBar = () => {
             <img src="/logo.png" alt="Claroid" />
             <h1 className="cursor-pointer">Clariod</h1>
           </div>
-          <div className="flex justify-center items-center">
-            <ul className=" hidden xl:flex items-center gap-4 font-semibold text-base">
-              <li className="p-3 hover:text-lightSlateGrey rounded-md transition-all cursor-pointer">
-                About
-              </li>
-              <li className="p-3 hover:text-lightSlateGrey rounded-md transition-all cursor-pointer">
-                Technology
-              </li>
-              <li className="p-3 hover:text-lightSlateGrey rounded-md transition-all cursor-pointer">
-                Press Kit
-              </li>
-              <li className="p-3 hover:text-lightSlateGrey rounded-md transition-all cursor-pointer">
-                Contact
-              </li>
-            </ul>
-          </div>
+          {getWidth.width < 1024 ? (
+            <Menu />
+          ) : (
+            <div className="flex justify-center items-center">
+              <ul className="lg:flex items-center gap-4 font-semibold text-base">
+                <li className="p-3 hover:text-lightSlateGrey rounded-md transition-all cursor-pointer">
+                  About
+                </li>
+                <li className="p-3 hover:text-lightSlateGrey rounded-md transition-all cursor-pointer">
+                  Technology
+                </li>
+                <li className="p-3 hover:text-lightSlateGrey rounded-md transition-all cursor-pointer">
+                  Press Kit
+                </li>
+                <li className="p-3 hover:text-lightSlateGrey rounded-md transition-all cursor-pointer">
+                  Contact
+                </li>
+              </ul>
+            </div>
+          )}
         </header>
       </nav>
     </>
