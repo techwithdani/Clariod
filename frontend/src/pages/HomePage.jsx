3e3d29b1e553ae40bc8110274adfc8a7944aca1c
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2025-03-07 16:27:53 +0500

    Fix paths

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,10 @@
-import AboutUsSection from "./components/AboutUsSection";
-import HeroSection from "./components/HeroSection";
-import NavBar from "./components/NavBar";
-import TechnologySection from "./components/TechnologySection";
-import QuoteSection from "./components/QuoteSection";
-import KeyBenefitsSection from "./components/KeyBenefitsSection";
-import Footer from "./components/Footer";
+import AboutUsSection from "../components/AboutUsSection";
+import HeroSection from "../components/HeroSection";
+import NavBar from "../components/NavBar";
+import TechnologySection from "../components/TechnologySection";
+import QuoteSection from "../components/QuoteSection";
+import KeyBenefitsSection from "../components/KeyBenefitsSection";
+import Footer from "../components/Footer";
 
 const HomePage = () => {
   return (
